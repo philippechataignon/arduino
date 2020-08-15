506b2a1a1882a113493129f561bc3412ceb1f6ef
Philippe Chataignon <philippe@ldlc>
2020-08-15 15:07:40 +0200

    Version timer1

--- timer/timer.ino
+++ timer/timer.ino
@@ -1,36 +1,28 @@
-#include <Time.h>
-#include <TimeAlarms.h>
-
-boolean doneOnce=false;
-
-void setup()
+/*
+ * Clignotement d'une DEL (LED en anglais) à 1 Hz par timer 2.
+ */
+const byte Led = 13; // Pour utiliser la LED du module
+#define LedToggle digitalWrite (Led, !digitalRead(Led))
+ 
+void setup ()
 {
-  Serial.begin(9600);
-  Serial.println("In Setup");
-  Alarm.timerOnce(10,OnceOnly); // called once after 10 seconds
-  return;
+  pinMode(Led, OUTPUT);
+  bitClear(TCCR2A, WGM20); // WGM20 = 0
+  bitClear(TCCR2A, WGM21); // WGM21 = 0 
+  TCCR2B = 0b00000110;      // Clock / 256 soit 16 micro-s et WGM22 = 0
+  TIFR2 = 0b00000001;       // TOV2
+  TCNT2 = 256 - 250;        // Chargement du timer à 6
 }
-
-void  loop()
-{
-  Alarm.delay(1000); // this function MUST be called repeatedly for the alarms/timers to work
-  if(!doneOnce)
-  {
-    doneOnce=true;
-    Serial.println("This message will only get printed one time");
+ 
+byte varCompteur = 0; // La variable compteur
+ 
+void loop () {
+  if (bitRead(TIFR2, 0) == 1) {       // Flag TOV2 mis à 1 ?
+    TCNT2 = 256 - 250;         // Rechargement du timer à 6
+    bitSet(TIFR2, TOV2);      // Remise à zéro du flag TOV2 (voir texte)
+    if (varCompteur++ > 125) { // Incrémentation et a atteint 125 ?
+      varCompteur = 0;         // On recommence un nouveau cycle
+      LedToggle;             // Inversion de la LED
+    }
   }
-  Alarm.timerOnce(10,NotSoGood); // alarm setup repeatedly
-  return;
-}
-
-void OnceOnly()
-{
-  Serial.println("This timer only triggers once");
-  return;
-}
-
-void NotSoGood()
-{
-  Serial.println("Alarm set in loop and so called repeatedly");
-  return;
 }
