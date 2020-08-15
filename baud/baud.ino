4db95e5a6cd4574e192234cf648238d7339a4389
Philippe Chataignon <philippe@ldlc>
2020-08-15 12:04:39 +0200

    First commit

---
+++ baud/baud.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(117500);
+}
+
+void loop() {
+  while ( Serial.available() ) {
+    Serial.read();
+  }
+}
---
+++ input/input.ino
@@ -0,0 +1,22 @@
+#define LEDPIN 12
+#define BUTPIN 10
+
+#define PAUSEON 500
+#define PAUSEOFF 500
+
+void setup() { 
+    Serial.begin(9600);
+    pinMode(LEDPIN, OUTPUT);
+    pinMode(BUTPIN, INPUT_PULLUP);
+} 
+
+void loop() { 
+    while (digitalRead(BUTPIN) == HIGH)
+        delay(50);
+    Serial.println("on");
+    digitalWrite(LEDPIN, HIGH);
+    while (digitalRead(BUTPIN) == LOW)
+        delay(50);
+    Serial.println("off");
+    digitalWrite(LEDPIN, LOW);
+}
