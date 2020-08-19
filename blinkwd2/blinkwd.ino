5894b6b09093bde86d697867281ba6363bd89a97
Philippe Chataignon <philippe@ldlc>
2020-08-19 10:03:05 +0200

    Add offset

--- blinkwd2/blinkwd.ino
+++ blinkwd2/blinkwd.ino
@@ -1,10 +1,12 @@
 class Button {
     int btnPin;      // the number of the LED pin
-    long OnTime;     // milliseconds of on-time
-    long IncrTime;
+    unsigned long OnTime;     // milliseconds of on-time
+    unsigned long IncrTime;
+    unsigned long Offset;
      
     // These maintain the current state
     int btnState;             		// btnState used to set the LED
+    int btnState0;             		// btnState used to set the LED
     int btnLevel;             		// btnLevel 0-255
     unsigned long previousMillis;  	// will store last time LED was updated
     unsigned long previousMillisLevel;  	// will store last time LED was updated
@@ -12,12 +14,13 @@ class Button {
   // Constructor - creates a Flasher 
   // and initializes the member variables and state
   public:
-  Button(int pin, long on, long incr = 10) {
+  Button(int pin, long on, long incr = 10, long offset = 0) {
     btnPin = pin;
     pinMode(btnPin, INPUT_PULLUP);     
     	  
     OnTime = on;
     IncrTime = incr;
+    Offset = offset;
     btnState = LOW; 
     btnLevel = 0; 
     previousMillis = 0;
@@ -27,7 +30,13 @@ class Button {
     // check to see if it's time to change the state of the LED
     unsigned long currentMillis = millis();
     if(digitalRead(btnPin) == LOW) {
+        btnState0 = HIGH; 
+        previousMillis = currentMillis;  // Remember the time
+    } 
+
+    if(btnState0 == HIGH && (currentMillis - previousMillis) >= Offset) {
         btnState = HIGH; 
+        btnState0 = LOW;
         previousMillis = currentMillis;  // Remember the time
     } 
          
@@ -59,8 +68,8 @@ class Flasher {
     // Class Member Variables
     // These are initialized at startup
     int ledPin;      // the number of the LED pin
-    long OnTime;     // milliseconds of on-time
-    long OffTime;    // milliseconds of off-time
+    unsigned long OnTime;     // milliseconds of on-time
+    unsigned long OffTime;    // milliseconds of off-time
      
     // These maintain the current state
     int ledState;             		// ledState used to set the LED
@@ -99,8 +108,8 @@ class Flasher {
 Flasher led1(13, 350, 350);
 // Flasher led2(12, 100, 400);
 Button btn1(10, 2000);
-Button btn2(10, 1000);
-Button btn3(10, 3000);
+Button btn2(10, 2000, 10, 1000);
+Button btn3(10, 2000, 10, 2000);
      
 void setup() {
     pinMode(3, OUTPUT);     
