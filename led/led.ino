eb6134497c200e97f0b053124ae2844c04cecfbc
Philippe Chataignon <philippe@ldlc>
2020-08-22 17:43:30 +0200

    Remove button in led

--- led/led.ino
+++ led/led.ino
@@ -1,18 +1,16 @@
 #define LEDPIN 12
-#define BUTPIN 10
 
 #define PAUSEON 500
 #define PAUSEOFF 500
 
 void setup() { 
     pinMode(LEDPIN, OUTPUT);
-    pinMode(BUTPIN, INPUT);
 } 
 
 void loop() { 
     // digitalWrite(LEDPIN, digitalRead(BUTPIN));
     digitalWrite(LEDPIN, LOW);
-    delay(500);
+    delay(2500);
     digitalWrite(LEDPIN, HIGH);
     delay(500);
 }
