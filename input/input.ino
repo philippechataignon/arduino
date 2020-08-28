0b9c6189e21d9f28ec584243956bcd54c7e006bd
Philippe Chataignon <philippe.chataignon@gmail.com>
2020-08-28 18:52:13 +0200

    dify input pgms

--- input/input.ino
+++ input/input.ino
@@ -1,5 +1,5 @@
-#define LEDPIN 12
-#define BUTPIN 10
+#define LEDPIN 13
+#define BUTPIN 2
 
 #define PAUSEON 500
 #define PAUSEOFF 500
@@ -12,12 +12,11 @@ void setup() {
 
 void loop() { 
     while (digitalRead(BUTPIN) == HIGH)
-        delay(50);
+        delay(10);
     Serial.println("on");
     digitalWrite(LEDPIN, HIGH);
-    delay(2000);
     while (digitalRead(BUTPIN) == LOW)
-        delay(50);
+        delay(10);
     Serial.println("off");
     digitalWrite(LEDPIN, LOW);
 }
--- input_intr/input_intr.ino
+++ input_intr/input_intr.ino
@@ -1,17 +1,27 @@
+volatile long deb_time = 0;
+volatile long cur_time = 0;
+
 void intr()
 {
-    digitalWrite(13, HIGH);
-    delay(1000);
+    cur_time = millis();
+    if ((cur_time - deb_time) > 200) {
+        Serial.println(">Intr");
+        digitalWrite(13, !digitalRead(13));
+    }
+    deb_time = millis();
 }
 
 void setup()
 {
     pinMode(13, OUTPUT);
-    attachInterrupt(0, intr, CHANGE);
+    pinMode(2, INPUT); // INT0 = pin 2 = PD2
+    attachInterrupt(0, intr, FALLING);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    digitalWrite(13, LOW);
+    Serial.println("Bip");
+    delay(1000);
 }
 
