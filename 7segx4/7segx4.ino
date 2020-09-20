39b39f4db17e71b14074eadad69d793fba876a08
Philippe Chataignon <philippe.chataignon@gmail.com>
2020-09-20 17:42:15 +0200

    Example for 7seg x 4

---
+++ 7segx4/7segx4.ino
@@ -0,0 +1,98 @@
+#define PAUSE 1000
+
+byte mask[] = {
+    0x3F,
+    0x06,
+    0x5B,
+    0x4F,
+    0x66,
+    0x6D,
+    0x7D,
+    0x07,
+    0x7F,
+    0x6F,
+    0x77,
+    0x7C,
+    0x39,
+    0x5E,
+    0x79,
+    0x71
+};
+
+void setup()
+{
+    for (int i = 1; i < 14; i++) {
+        pinMode(i, OUTPUT);
+    }
+    for (int i = 10; i < 14; i++) {
+        digitalWrite(i, HIGH);
+    }
+}
+
+void loop()
+{
+    for (int i = 1; i < 8; i++) {
+        digitalWrite(i, LOW);
+    }
+    int n = 7;
+    digitalWrite(10, LOW);
+    byte m = mask[n];
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(i + 1, bitRead(m, i));
+    }
+    delay(1);
+    digitalWrite(10, HIGH);
+    digitalWrite(11, LOW);
+    n++;
+    m = mask[n];
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(i + 1, bitRead(m, i));
+    }
+    delay(1);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, LOW);
+    n++;
+    m = mask[n];
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(i + 1, bitRead(m, i));
+    }
+    delay(1);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    n++;
+    m = mask[n];
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(i + 1, bitRead(m, i));
+    }
+    delay(1);
+    digitalWrite(13, HIGH);
+//#        n++;
+//#        digitalWrite(11, HIGH);
+//#        delay(10);
+//#        m = mask[n];
+//#        for (int i = 0; i < 7; i++) {
+//#            digitalWrite(i + 1, bitRead(m, i));
+//#        }
+//#        delay(10);
+//#        digitalWrite(11, LOW);
+//#        n++;
+//#        digitalWrite(12, HIGH);
+//#        delay(10);
+//#        m = mask[n];
+//#        for (int i = 0; i < 7; i++) {
+//#            digitalWrite(i + 1, bitRead(m, i));
+//#        }
+//#        delay(10);
+//#        digitalWrite(12, LOW);
+//#        n++;
+//#        digitalWrite(13, HIGH);
+//#        delay(10);
+//#        m = mask[n];
+//#        for (int i = 0; i < 7; i++) {
+//#            digitalWrite(i + 1, bitRead(m, i));
+//#        }
+//#        delay(10);
+//#        digitalWrite(13, LOW);
+//#        delay(PAUSE);
+//#    }
+}
